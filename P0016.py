1e0dbbe08f36369136798bdcb7c872283546078b
hexapetalous <hexapetalous@126.com>
2016-03-07 23:15:51 +0800

    Seats model.

--- P0016.py
+++ P0016.py
@@ -1,12 +1,38 @@
 from flask import Flask
 from flask import render_template
+from seatmap import SeatMap
+
+CLASSROOM_WIDTH = 19
+CLASSROOM_HEIGHT = 9
 
 app = Flask(__name__)
+seatmap = SeatMap(CLASSROOM_WIDTH, CLASSROOM_HEIGHT)
+# seatmap.get_seat(2, 3).set_shadowed()
+
+
+def format_seat_map(seatmap):
+    myMap = []
+    for y in range(CLASSROOM_HEIGHT):
+        row = []
+        for x in range(CLASSROOM_WIDTH):
+            seat1 = {}
+            seat2 = seatmap.get_seat(x, y)
+            if seat2.is_shadowed():
+                seat1['shadowed'] = 1
+            else:
+                seat1['shadowed'] = 0
+                seat1['best'] = seat2.get_best_choice_list()
+                seat1['soso'] = seat2.get_soso_choice_list()
+            row.append(seat1)
+        myMap.append(row)
+    return myMap
 
 
 @app.route('/')
 def hello_world():
-    return render_template('app.html')
+    global seatmap
+    sm = format_seat_map(seatmap)
+    return render_template('app.html', seatmap=sm)
 
 
 if __name__ == '__main__':
