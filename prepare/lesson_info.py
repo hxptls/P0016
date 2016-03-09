0d15592b890d03a40b08fa7c9273df06b65736c4
hexapetalous <hexapetalous@126.com>
2016-03-10 06:34:33 +0800

    So tired.

---
+++ prepare/lesson_info.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+lesson_info = {
+  "subject":  u"工科分析",
+  "location": u"D405",
+  "teacher":  u"申建中",
+  "time":     u"2016.3.14 星期一 1~2节"
+}
---
+++ prepare/system_info.py
@@ -0,0 +1,3 @@
+system_info = {
+    'version': '0.1-alpha.20160310'
+}
