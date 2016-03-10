6939bffad8c1b1914cb8fcaf8f3ae6a91e874f13
hexapetalous <hexapetalous@126.com>
2016-03-10 17:02:56 +0800

    Features.

--- prepare/system_info.py
+++ prepare/system_info.py
@@ -1,3 +1,8 @@
+# coding=utf-8
 system_info = {
-    'version': '0.1-alpha.20160310'
+    'version': '0.1-alpha.20160310',
+    'feature': [
+        u'发布了算新特性嘛……',
+        u'加入了新特性这个模块……'
+    ]
 }
