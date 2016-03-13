ce841254b177ec85cdf7ec2ce3a911fcbcbad0e5
hexapetalous <hexapetalous@126.com>
2016-03-13 19:07:11 +0800

    Dy-width container now!

---
+++ static/setwidth.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wan on 3/13/16.
+ */
+$('#classroom-container').ready(function() {
+    var maxRight = 0;
+    $('.classroom-seat').each(function(n, ele) {
+        maxRight = Math.max(maxRight, ele.offsetLeft + ele.offsetWidth);
+    });
+    $('.classroom-row').each(function(n, ele) {
+        ele.style.width = '' + (maxRight - 10) + 'px';
+    });
+});
